c7403b9cd0ae1ee4713766164a6b85a4723c2433
mgalihpp <muhammadgalih451@gmail.com>
2023-11-20 13:02:25 +0700

    Add new import for Footer component in layout.jsx and add Footer component after {children} in the return statement. Import Search, Image, and Link components, and styles module in users/page.jsx. Replace <div>UsersPage</div> with the actual JSX code for the UsersPage component.

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.btn}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.users}>
+                <Image
+                  src="/noavatar.png"
+                  alt="profile"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+              </div>
+            </td>
+            <td>muhamm@gmail.com</td>
+            <td>22.02.2022</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view} `}>
+                  View
+                </button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete} `}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
