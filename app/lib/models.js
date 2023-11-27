1f52fd4fb8ada66eccb302024b2f61faca958d38
mgalihpp <muhammadgalih451@gmail.com>
2023-11-27 14:39:30 +0700

    feat: Add fetchUsers function and related dependencies
    This change adds a new file, `data.js`, which contains a `fetchUsers`
    function for retrieving user data from the database using Mongoose. The
    function connects to the database, fetches the users, and returns them.
    If there is an error, it throws an error message.
    
    This addition was necessary to enable the retrieval of user data in the
    `UsersPage` component in `page.jsx`. The async `UsersPage` function now
    fetches the users using `fetchUsers` and logs them to the console.

---
+++ app/lib/models.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    img: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    size: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model.User || mongoose.model("Users", userSchema);
+export const Product =
+  mongoose.model.Product || mongoose.model("Product", productSchema);
